bc990da2e222b4f0d7fb4e47cc4570093acb87cd
Ayhan <ayhaneyikan@outlook.com>
2023-11-01 00:10:34 -0400

    moving state enum to common module

---
+++ common/src/state.rs
@@ -0,0 +1,8 @@
+/// defines the states a control service transitions through during execution
+#[derive(Debug, Clone, PartialEq)]
+pub enum State {
+    CREATED,
+    ISSUED,
+    EXECUTING,
+    COMPLETED,
+}
