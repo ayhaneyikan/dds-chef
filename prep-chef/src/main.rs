8a347a7391b284e0f97a2da369369edf59779b89
Ayhan Eyikan <aeyikan@umd.edu>
2023-11-23 10:31:57 -0500

    implementing control loop message sending

--- prep-chef/src/main.rs
+++ prep-chef/src/main.rs
@@ -9,6 +9,13 @@ fn main() {
     println!("Prep chef beginning preparations");
     while !pc.check_completed() {
         pc.cycle();
+        if pc.check_failed() {
+            println!(
+                "Head chef has failed with no option for recovery: {}",
+                pc.get_failure_msg().unwrap()
+            );
+            return;
+        }
     }
 
     println!("Prep chef completed preparations");
--- prep-chef/src/prep_chef.rs
+++ prep-chef/src/prep_chef.rs
@@ -1,34 +1,60 @@
 use std::{thread::sleep, time::Duration};
 
-use common::{io::Receiver, msgs::PrepareCommand, state::State, steps::FoodItem};
+use common::{
+    io::{Receiver, Sender},
+    msgs::{CommandDone, PrepareCommand, PrepareCommandAck},
+    state::State,
+    steps::FoodItem,
+};
 
 pub struct PrepChefService {
-    current_state: State,
-    command_receiver: Receiver<PrepareCommand>,
     prep_item: Option<FoodItem>,
+    service_state: State,
+    command_receiver: Receiver<PrepareCommand>,
+    command_ack_sender: Sender<PrepareCommandAck>,
+    command_done_sender: Sender<CommandDone>,
 }
 
 impl PrepChefService {
     /// Creates new PrepChefService instance
     pub fn new() -> Self {
         Self {
-            current_state: State::CREATED,
-            command_receiver: Receiver::new("begin_preparation_command".to_string(), None),
             prep_item: None,
+            service_state: State::CREATED,
+            command_receiver: Receiver::new("prepare_command".to_string(), None),
+            command_ack_sender: Sender::new("prepare_command_ack".to_string(), None),
+            command_done_sender: Sender::new("prepare_command_done".to_string(), None),
         }
     }
 
+    /// Returns boolean indicating completion status of the service
     pub fn check_completed(&self) -> bool {
-        matches!(self.current_state, State::COMPLETED)
+        matches!(self.service_state, State::COMPLETED)
+    }
+    /// Returns boolean indicating whether the service failed while "cooking"
+    pub fn check_failed(&self) -> bool {
+        matches!(self.service_state, State::FAILED(_))
+    }
+    /// Returns string containing explanation of reason for failure
+    /// ### Returns
+    /// - `None` if service is not in `FAILED` state
+    /// - `Some(message)` describing the failure if `FAILED`
+    pub fn get_failure_msg(&self) -> Option<&str> {
+        if let State::FAILED(error_message) = &self.service_state {
+            return Some(error_message);
+        }
+        None
     }
 
     pub fn cycle(&mut self) {
-        match self.current_state {
+        match self.service_state {
             State::CREATED => self.on_created(),
             State::ISSUED => self.on_issued(),
             State::EXECUTING => self.on_executing(),
             // shouldn't enter cycle() while COMPLETED
             State::COMPLETED => (),
+            // failures checked and printed in main loop
+            State::FAILED(_) => (),
         }
     }
 
@@ -36,7 +62,13 @@ impl PrepChefService {
     fn on_created(&mut self) {
         if let Some(command) = self.command_receiver.receive() {
             self.prep_item = Some(command.get_item());
-            self.current_state = State::ISSUED;
+            self.service_state = State::ISSUED;
+            // send ack
+            self.command_ack_sender
+                .send(PrepareCommandAck)
+                .unwrap_or_else(|e| {
+                    self.service_state = State::FAILED(format!("Failed to send ack: {}", e));
+                });
         }
     }
 
@@ -56,7 +88,7 @@ impl PrepChefService {
             "Prep materials gathered for the {}",
             self.prep_item.unwrap()
         );
-        self.current_state = State::EXECUTING;
+        self.service_state = State::EXECUTING;
     }
 
     /// After initialization, perform necessary preparation task
@@ -71,7 +103,14 @@ impl PrepChefService {
                 sleep(Duration::from_secs(3));
             }
         }
+
         println!("Preparations completed");
-        self.current_state = State::COMPLETED;
+        self.service_state = State::COMPLETED;
+        // send done indicator
+        self.command_done_sender
+            .send(CommandDone)
+            .unwrap_or_else(|e| {
+                self.service_state = State::FAILED(format!("Failed to send done: {}", e));
+            });
     }
 }
